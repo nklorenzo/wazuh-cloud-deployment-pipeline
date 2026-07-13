71fd92158274b5529de3c53fb1270abca1a4f66d
nklorenzo <etoundinkodolaurenbrian@gmail.com>
2026-07-13 15:19:16 +0100

    refactor: traduire les descriptions du playbook en français et supprimer le backend S3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,14 +5,6 @@ terraform {
       version = "6.54.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "projet-stage-tfstate-336471570575"
-    key            = "projet-stage/terraform.tfstate"
-    region         = "eu-north-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
 }
 
 provider "aws" {
