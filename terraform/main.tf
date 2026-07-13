50ba0c0b98be5895b1ac992c276091481525322f
nklorenzo <etoundinkodolaurenbrian@gmail.com>
2026-07-13 14:10:33 +0100

    feat: add S3 backend, Tailscale install, security group rules and improve playbook

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,6 +31,14 @@ resource "aws_security_group" "ssh_only" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Tailscale"
+    from_port   = 41641
+    to_port     = 41641
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "6.54.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "projet-stage-tfstate-336471570575"
+    key            = "projet-stage/terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
