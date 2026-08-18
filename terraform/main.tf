1f74304769ca60a0f56ac326665052b5759ab17a
nklorenzo <etoundinkodolaurenbrian@gmail.com>
2026-08-18 10:27:19 +0100

    fix: réouvrir SSH sur 0.0.0.0/0 pour le runner CI/CD

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,11 +8,11 @@ resource "aws_security_group" "ssh_only" {
   description = "autoriser uniquement le trafic SSH"
 
   ingress {
-    description = "SSH via Tailscale uniquement"
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["100.64.0.0/10"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 
